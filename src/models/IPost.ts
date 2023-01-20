0b9d1fbb9482ca4dfd1a3b16fd1c0f7e59f8c68d
Raman Krytski <rkeydevelop@gmail.com>
2023-01-20 17:59:41 +0100

    created first models

---
+++ src/models/IPost.ts
@@ -0,0 +1,5 @@
+export interface IPost {
+    id: number;
+    authorId: number;
+    message: string;
+}
---
+++ src/models/IUser.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+    id: number;
+    login: string;
+    email: string;
+    password: string;
+    firstName: string;
+    secondName: string;
+}
